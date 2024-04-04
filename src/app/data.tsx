26e8ad501e9e97e6bbdd53fe6f0bdafbb168b666
kat369 <kathiravanarjunan369@gmail.com>
2024-04-04 17:03:45 +0530

    last

---
+++ src/app/data.tsx
@@ -0,0 +1,56 @@
+export const items = [
+  {
+    name: "hotel1",
+    category: "motel",
+    rating:3,
+    aminities:["ac", "tv","washing","wifi","food"]
+  },
+  {
+    name: "hotel2",
+    category: "suithotel",
+    rating:4,
+    aminities:["ac", "tv","washing","wifi","food"]
+  },
+  {
+    name: "hotel3",
+    category: "residancy",
+    rating:5,
+    aminities:["ac", "tv","washing","wifi","food"]
+  },
+  {
+    name: "hotel4",
+    category: "inn",
+    rating:3,
+    aminities:["ac", "tv","washing","wifi","food"]
+  },
+  {
+    name: "hotel5",
+    category: "motel",
+    rating:3,
+    aminities:["ac", "tv","washing","wifi","food"]
+  },
+  {
+    name: "hotel6",
+    category: "residancy",
+    rating:4,
+    aminities:["ac", "tv","washing","wifi","food"]
+  },
+  {
+    name: "hotel7",
+    category: "inn",
+    rating:2,
+    aminities:["ac", "tv","washing","wifi","food"]
+  },
+  {
+    name: "hotel8",
+    category: "motel",
+    rating:3,
+    aminities:["ac", "tv","washing","wifi","food"]
+  },
+  {
+    name: "hotel9",
+    category: "suithotel",
+    rating:3,
+    aminities:["ac", "tv","washing","wifi","food"]
+  },
+];
